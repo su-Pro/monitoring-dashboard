9242e24339234198c45f348bc1ccf9a4112c25a2
wurenbieke <wurenbieke@bytedance.com>
2022-05-31 11:02:04 +0800

    feat: ts支持

--- config-overrides.js
+++ config-overrides.js
@@ -1,12 +1,15 @@
 const path = require(`path`);
 const alias = require(`./aliases`);
-const { aliasWebpack } = require('react-app-alias');
+const { aliasWebpack } = require("react-app-alias");
 
 const SRC = `./src`;
 const aliases = alias(SRC);
 
 const resolvedAliases = Object.fromEntries(
-  Object.entries(aliases).map(([key, value]) => [key, path.resolve(__dirname, value)])
+  Object.entries(aliases).map(([key, value]) => [
+    key,
+    path.resolve(__dirname, value),
+  ])
 );
 
 const options = {
